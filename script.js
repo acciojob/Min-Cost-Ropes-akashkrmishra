d50d9903cc1ee3ef2efd6357de03b66758610b2d
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2024-06-17 00:59:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function mincost(arr)
 	let m=arr.length;
 //write your code here
 // return the min cost
-	for(let i=0;i<m;i++){
+	for(let i=0;i<m-1;i++){
 		 arr.sort((a,b) => a-b);
 	cost+=arr[0]+arr[1];
 	arr.splice(0,2,cost);
