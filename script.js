bcfa7ae8d2ef0a67f7fdac446d7939aee7c6d382
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2024-06-16 15:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+let cost=0;
 function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  arr.sort((a,b) => a-b);
+	for(let i=0;i<arr.length-1;i++){
+	cost=arr[0]+arr[1];
+	arr.splice(0,2,cost);
+	}
+	return cost;
 }
 
 module.exports=mincost;
