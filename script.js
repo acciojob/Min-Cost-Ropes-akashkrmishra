27322cb16d53652b043fa8777222bf1baaa8f2c1
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2024-06-17 13:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-let cost=0;
-function mincost(arr)
-{ 
-	let m=arr.length;
-//write your code here
-// return the min cost
-	for(let i=0;arr.length>1;i++){
-		 arr.sort((a,b) => a-b);
-	cost+=arr[0]+arr[1];
-	arr.splice(0,2,cost);
-	}
-	return cost;
-}
+// let cost=0;
+// function mincost(arr)
+// { 
 
-module.exports=mincost;
+// //write your code here
+// // return the min cost
+// 	for(let i=0;arr.length>1;i++){
+// 		 arr.sort((a,b) => a-b);
+// 	cost+=arr[0]+arr[1];
+// 	arr.splice(0,2,cost);
+// 	}
+// 	return cost;
+// }
+
+// module.exports=mincost;
+
+
+
+function mincost(arr) {
+    let cost = 0;
+    arr.sort((a, b) => a - b);
+    while (arr.length > 1) {
+        const newRope = arr.shift() + arr.shift();
+        cost += newRope;
+        arr.push(newRope);
+        arr.sort((a, b) => a - b);
+    }
+    return cost;
+}
+console.log(mincost([7,3,5,9,4])); 
+console.log(mincost([1, 2, 3, 4, 5])); 
+module.exports = mincost;
+
